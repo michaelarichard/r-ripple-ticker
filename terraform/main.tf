9b3117eafbec9415980cdcf717810d4d16577aab
mrichard <michaelarichard@gmail.com>
2018-01-28 20:28:39 -0800

    Updating filename

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,11 +27,11 @@ EOF
 }
 
 resource "aws_lambda_function" "ticker_lambda" {
-  filename         = "lambda_function.zip"
+  filename         = "lambda.py.zip"
   function_name    = "${var.function_name}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "lambda_function.lambda_handler"
-  source_code_hash = "${base64sha256(file("lambda_function.zip"))}"
+  source_code_hash = "${base64sha256(file("lambda.py.zip"))}"
   runtime          = "python3.6"
   environment {
     variables = {
