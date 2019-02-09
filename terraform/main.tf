35b38fe9547e3e5bfb9e70ca44af5adc0a925bf3
mrichard <michaelarichard@gmail.com>
2019-02-09 14:47:15 -0800

    10 minute

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "ticker_lambda" {
 resource "aws_cloudwatch_event_rule" "ticker_x_min" {
    name = "ticker_x_min"
    depends_on = ["aws_lambda_function.ticker_lambda"]
-   schedule_expression = "rate(1 minute)"
+   schedule_expression = "rate(${var.SCHEDULE})"
 }
 
 resource "aws_cloudwatch_event_target" "ticker_x_min" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,4 +3,5 @@ variable "REDDIT_CLIENT_SECRET" {}
 variable "REDDIT_CLIENT_USERNAME" {}
 variable "REDDIT_CLIENT_PASSWORD" {}
 variable "REDDIT_SUBREDDIT" {}
+variable "SCHEDULE" { default = "1 minute"}
 variable "region" {}
