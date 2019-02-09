feda726e829e50e770736f6a5835d74819758717
mrichard <michaelarichard@gmail.com>
2019-02-08 21:57:12 -0800

    Adding variable REDDIT_SUBREDDIT

---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "REDDIT_CLIENT_ID" {}
+variable "REDDIT_CLIENT_SECRET" {}
+variable "REDDIT_CLIENT_USERNAME" {}
+variable "REDDIT_CLIENT_PASSWORD" {}
+variable "REDDIT_SUBREDDIT" {}
+variable "region" {}
+variable "function_name" {}
