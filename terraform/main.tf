f8dc210a216a225d3b67c85a1abba3cad62a9249
mrichard <michaelarichard@gmail.com>
2019-02-08 23:12:49 -0800

    re-using existing role

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
+#resource "aws_iam_role" "iam_for_lambda" {
+#  name = "iam_for_lambda"
+#
+#  assume_role_policy = <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Action": "sts:AssumeRole",
+#      "Principal": {
+#        "Service": "lambda.amazonaws.com"
+#      },
+#      "Effect": "Allow",
+#      "Sid": ""
+#    }
+#  ]
+#}
+#EOF
+#}
 
 resource "aws_lambda_function" "ticker_lambda" {
   filename         = "lambda.py.zip"
   function_name    = "${var.function_name}"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  role             = "${data.aws_iam_role.reddit_lambda.arn}"
   handler          = "lambda_function.lambda_handler"
   source_code_hash = "${base64sha256(file("lambda.py.zip"))}"
   runtime          = "python3.6"
---
+++ terraform/role.tf
@@ -0,0 +1,3 @@
+data "aws_iam_role" "reddit_lambda" {
+  name = "myRedditRole"
+}
