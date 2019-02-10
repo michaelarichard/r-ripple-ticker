57ef4d48d021efa6f76a431691fcffd640846677
mrichard <michaelarichard@gmail.com>
2019-02-09 16:41:19 -0800

    state and tf refactoring

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,5 @@
 terraform {
   backend "s3" {
-    bucket = "stormpath-terraform-state"
     region = "us-west-2"
-    key = "rripple/ticker/us-west-2/${var.REDDIT_SUBREDDIT}/terraform.tfstate"
   }
 }
