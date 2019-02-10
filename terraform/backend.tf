2cd6f2e0630ad8c70d2b7bc387abdafefd312ad5
mrichard <michaelarichard@gmail.com>
2019-02-09 16:11:14 -0800

    Variable backend key

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "stormpath-terraform-state"
     region = "us-west-2"
-    key = "rripple/ticker/us-west-2/prod/terraform.tfstate"
+    key = "rripple/ticker/us-west-2/${var.REDDIT_SUBREDDIT}/terraform.tfstate"
   }
 }
